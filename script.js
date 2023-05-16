ec8843aa5adad4391b1ddab1b3993cf9ba091264
KaeTheDev <kaethedev@gmail.com>
2023-05-16 19:47:59 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* SELECT A CITY */
+
+function selectCity() {
+    alert("Loading weather report...")
+}
+
+
+/* REMOVE COOKIE WARNING */
+
+var acceptBtn = document.querySelector(".cookie-warning");
+
+function removeCookieWarning() {
+    acceptBtn.remove();
+}
